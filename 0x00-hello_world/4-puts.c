3d787e69847f6d728501f07b85ead10b44393921
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-12 18:29:25 +0000

    new update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-#include<stdout.h>
 /**
-*main - entry-point
-*return: always zer (0) (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-
 }
