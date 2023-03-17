dd564f17f939dc9b6d80c175c239266648a2604a
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-17 17:51:00 +0000

    new update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+
+/**
+ * main - it Print out lower_case  alphabets in reversed form.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
