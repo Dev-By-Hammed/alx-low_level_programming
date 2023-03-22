29faaf1f5185570c3f49d6576349419f84c7fbee
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-22 08:58:58 +0000

    new update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,15 +13,15 @@ int largest_number(int a, int b, int c)
 	int largest;
 	if (a >= b && a >= c)
 	{
-		largest[] = a;
+		int largest = a;
 	}
 	else if (b >= a && b >= c)
 	{
-		largest[] = b;
+		int largest = b;
 	}
 	else
 	{
-		largest[] = c;
+		int largest = c;
 	}
 	return (largest);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+this is the begining of c and the first directory in c in alx SE program
