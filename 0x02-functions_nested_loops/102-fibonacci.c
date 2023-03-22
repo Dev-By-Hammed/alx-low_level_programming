32e4d000af2d5752a5b644fa503c3e58812b7aae
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-22 14:02:50 +0000

    new update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - print out first 50 fibinacci numbers
+ *Return: Return 0.
+ */
+int main(void)
+{
+	int c;
+
+	long int n1, n2, n3, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf("%ld", fn);
+		n1 = n2;
+		n2 = fn;
+
+	}
+	printf("\n");
+	return (0);
+}
