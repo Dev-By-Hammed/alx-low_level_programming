073cfb2a04e316ccb6fa22f4fc43a2d228f27a56
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-23 10:51:50 +0000

    new update

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Fileme	          Description
+
+0-isupper.c:	 it Chekcks for uppercase character (it retuen 1 if c is uppercase and 0 if not 
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ *_putchar - writes the character c into stdout.
+ *@c: is the character to printout.
+ *Return: 1 if success.
+ *return -1 if there is error 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
