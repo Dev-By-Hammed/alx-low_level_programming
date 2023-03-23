52626a0ecc9cae327430ad1c0bcb87fd927c421b
Dev-By-Hammed <hammedakinwale35@gmail.com>
2023-03-23 12:03:53 +0000

    new update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isudigit - check for a number between 0 and 9.
+ *_isdigit - check for a number between 0 and 9.
  *@c: input number.
  *Return: 1 if it's a number between 0 and 9, and 0 in different case.
  */
